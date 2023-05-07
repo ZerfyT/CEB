738180945bea429869970579b8317485eb93f427
ZerfyT <nipdili1143@gmail.com>
2023-05-07 16:43:43 +0530

    Add user roles and permissions

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('nic');
+            $table->integer('account_number');
+            $table->string('account_type')->nullable();
+            $table->string('area')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Permission::truncate();
+        Role::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permissions = [
+            ['name' => 'list-users'],
+            ['name' => 'delete-users'],
+
+            ['name' => 'change-role'],
+            ['name' => 'list-role'],
+            ['name' => 'create-role'],
+            ['name' => 'edit-role'],
+            ['name' => 'delete-role'],
+
+            ['name' => 'list-permission'],
+            ['name' => 'create-permission'],
+            ['name' => 'edit-permission'],
+            ['name' => 'delete-permission'],
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' =>$permission['name']]);
+        }
+
+        $superAdminRole = Role::create(['name' => 'super-admin']);
+        $adminRole = Role::create(['name' => 'admin']);
+        $cashier = Role::create(['name' => 'cashier']);
+        $meterReader = Role::create(['name' => 'meter-reader']);
+        $userRole = Role::create(['name' => 'user']);
+
+        $superAdminRole->givePermissionTo(Permission::all());
+        $adminRole->givePermissionTo('list-users');
+    }
+}
