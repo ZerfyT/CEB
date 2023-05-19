784a8af19cc4f50831c681c0091e7f304f08b71e
ZerfyT <nipdili1143@gmail.com>
2023-05-20 00:08:45 +0530

    Redesign homepage

--- resources/views/mreader/home.blade.php
+++ resources/views/mreader/home.blade.php
@@ -6,72 +6,83 @@
 
 @section('content')
     <div class="container py-3">
-        <label class="form-control" for="floatingInput">Home</label> <br>
-        <h2 class="fw-bold">Home</h2>
+
+        {{-- Breadcrumbs --}}
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ route('mreader.home') }}">Home</a></li>
+            </ol>
+        </nav>
+
+        {{-- Heading --}}
+        <h2 class="fw-bold">Dashboard</h2>
 
         {{-- Cards --}}
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-header">
-                            Featured
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
+        <div class="container py-3">
+            <div class="col">
+                <div class="row">
 
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-header">
-                            Featured
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    <!-- Customers Card -->
+                    <div class="col-xxl-4 col-md-6">
+                        <div class="card sales-card">
 
-            <div class="row py-4">
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-header">
-                            Featured
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Customers <span>| Total</span></h5>
 
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-header">
-                            Featured
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-people"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>145</h6>
+                                        {{-- <span class="text-success small pt-1 fw-bold">12%</span> --}}
+                                        {{-- <span class="text-muted small pt-2 ps-1">increase</span> --}}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
+                    <!-- Readings Card -->
+                    <div class="col-xxl-4 col-md-6">
+                        <div class="card revenue-card">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Readings <span>| This Month</span></h5>
+
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-pencil-square"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>24</h6>
+                                        <span class="text-success small pt-1 fw-bold">8%</span>
+                                        <span class="text-muted small pt-2 ps-1">completed</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Readings All Card -->
+                    <div class="col-xxl-4 col-xl-12">
+                        <div class="card customers-card">
+                            <div class="card-body">
+                                <h5 class="card-title">Readings <span>| Total</span></h5>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-pencil-square"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>1244</h6>
+                                        <span class="text-danger small pt-1 fw-bold">12%</span> <span
+                                            class="text-muted small pt-2 ps-1"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
