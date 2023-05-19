f1724f5682223286412e8d06ee6efddb7de8738d
ZerfyT <nipdili1143@gmail.com>
2023-05-20 00:13:26 +0530

    Uncomment cashier routes

--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,18 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
 
-    // Route::controller(CashierController::class)->group(function () {
-    //     Route::get('/', 'cashierHomepage')->name('home');
-    //     Route::get('/payment', 'cashierPayments')->name('payment-home');
-    //     Route::get('/payment/customer-bill', 'cashierCustomerBill')->name('customer-bill');
-    //     Route::get('/payment/genarate-bill', 'cashierGenarateBill')->name('genarate-bill');
-    //     Route::get('/payment/paybill', 'cashierPay')->name('paybill');
-    //     Route::get('/payment/receipt', 'cashierReceipt')->name('payment-receipt');
-    //     Route::get('profile', 'cashierProfile')->name('profile');
-    //     Route::get('/payment-history', 'cashierPaymentHistory')->name('payment-history');
-    //     Route::get('email-history', 'cashierEmail')->name('email-history');
-    //     Route::get('/user', 'getUser')->name('user');
-    // });
+    Route::controller(CashierController::class)->group(function () {
+        Route::get('/', 'cashierHomepage')->name('home');
+        Route::get('/payment', 'cashierPayments')->name('payment-home');
+        Route::get('/payment/customer-bill', 'cashierCustomerBill')->name('customer-bill');
+        Route::get('/payment/genarate-bill', 'cashierGenarateBill')->name('genarate-bill');
+        Route::get('/payment/paybill', 'cashierPay')->name('paybill');
+        Route::get('/payment/receipt', 'cashierReceipt')->name('payment-receipt');
+        Route::get('profile', 'cashierProfile')->name('profile');
+        Route::get('/payment-history', 'cashierPaymentHistory')->name('payment-history');
+        Route::get('email-history', 'cashierEmail')->name('email-history');
+        Route::get('/user', 'getUser')->name('user');
+    });
 
 
     Route::controller(MReaderController::class)->group(function() {
