c5fd0068ea49298b65c6c6797222469b35ac2f3c
Adarsha Thiwangana <adarsha1thiwangana@gmail.com>
2023-05-20 08:55:48 +0530

    Initial Commit

--- resources/views/cashier/payments/customer-bill.blade.php
+++ resources/views/cashier/payments/customer-bill.blade.php
@@ -7,28 +7,30 @@
 @section('content')
     <label class="form-control" for="floatingInput">Payment</label> <br>
     <h2 class="fw-bold">Payment</h2>
-    
+
     {{-- Table --}}
     <div class="container py-4">
         <table class="table table-sm table-bordered table-hover">
             <thead>
-              <tr class="bg-secondary">
-                <th class="col-1">ID</th>
-                <th class="col-3">Account No</th>
-                <th class="col-3">Date</th>
-                <th class="col-3">Address</th>
-                <th class="col-2">Balance</th>
-              </tr>
+                <tr class="bg-secondary">
+                    <th class="col-1">ID</th>
+                    <th class="col-3">User Id</th>
+                    <th class="col-3">Date</th>
+                    <th class="col-3">Balance</th>
+                    <th class="col-2">Status</th>
+                </tr>
             </thead>
             <tbody class="table-light">
-              <tr onclick="window.location.href='{{ route('genarate-bill') }}'">
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td> 
-                <td>@mdo</td>
-                <td>10000</td>
-              </tr>
-              <tr onclick="window.location.href='{{ route('genarate-bill') }}'">
+                @foreach ($bills as $bill)
+                    <tr onclick="window.location.href='{{ route('genarate-bill') }}'">
+                        <th scope="row">{{ $bill->id }}</th>
+                        <td>{{ $bill->user_id }}</td>
+                        <td>{{ $bill->date }}</td>
+                        <td>{{ $bill->amount }}</td>
+                        <td>{{ $bill->status }}</td>
+                    </tr>
+                     @endforeach
+                    {{-- <tr onclick="window.location.href='{{ route('genarate-bill') }}'">
                 <th scope="row">2</th>
                 <td>Jacob</td>
                 <td>Thornton</td>
@@ -41,8 +43,8 @@
                 <td>@twitter</td>
                 <td>@mdo</td>
                 <td>2000</td>
-              </tr>
+              </tr> --}}
             </tbody>
-          </table>
+        </table>
     </div>
 @endsection
