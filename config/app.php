d271b7caa2efce8d3c7d2ed340f272b7c0c6087c
ZerfyT <nipdili1143@gmail.com>
2023-05-30 23:09:59 +0530

    Install barryvdh's Debugbar for Laravel

--- config/app.php
+++ config/app.php
@@ -170,7 +170,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
-
     ])->toArray(),
 
     /*
