83766a492f9a313a8f74a0d1d76244bbbadaaaf1
Adarsha Thiwangana <adarsha1thiwangana@gmail.com>
2023-06-01 11:38:45 +0530

    google login

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,9 @@ Route::get('auth/google', [GoogleAuthController::class,'redirect'])->name('googl
 Route::get('auth/google/callback', [GoogleAuthController::class,'callbackGoogle']);
 
 
+Route::get('auth/google', [GoogleAuthController::class,'redirect'])->name('google-auth');
+Route::get('auth/google/callback', [GoogleAuthController::class,'callbackGoogle']);
+
 Route::get('auth/facebook', [FacebookAuthController::class,'redirect'])->name('facebook-auth');
 Route::get('auth/facebook/callback', [FacebookAuthController::class,'facebookCallback']);
 
