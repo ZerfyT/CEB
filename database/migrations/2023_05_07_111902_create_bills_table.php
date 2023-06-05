dde41b32c826f0ed022afcadf13ce2409d4c8059
Adarsha Thiwangana <adarsha1thiwangana@gmail.com>
2023-06-05 15:37:01 +0530

    Migrate changes

--- database/migrations/2023_05_07_111902_create_bills_table.php
+++ database/migrations/2023_05_07_111902_create_bills_table.php
@@ -14,12 +14,15 @@ return new class extends Migration
         Schema::create('bills', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->boolean('status');
+            $table->boolean('status')->default(false);
             $table->integer('old_reading');
             $table->integer('new_reading');
             $table->date('old_reading_date');
             $table->date('new_reading_date');
             $table->integer('units');
+            $table->decimal('range_one_cost');
+            $table->decimal('range_two_cost');
+            $table->decimal('range_three_cost');
             $table->decimal('charge_fixed');
             $table->decimal('charge_for_units');
             $table->decimal('charge_for_month');
